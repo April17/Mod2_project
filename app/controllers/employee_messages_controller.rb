b83397d4aa36f4e5e6773e9e05a2273fd27e9cdd
April17 <fanmeng0417@gmail.com>
2019-06-20 17:46:21 -0400

    Add Message

--- app/controllers/employee_messages_controller.rb
+++ app/controllers/employee_messages_controller.rb
@@ -1,2 +1,13 @@
 class EmployeeMessagesController < ApplicationController
+
+  def create
+    return_path = Employee.find_by(username: session[:user_username])
+    EmployeeMessage.create(employee_message_params)
+    redirect_to return_path
+  end
+
+end
+
+def employee_message_params
+  params.require(:employee_message).permit(:title, :content, :sender_id, :receiver_id)
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,7 @@ class Employee < ApplicationRecord
   has_many :tasks, through: :employee_tasks
   has_many :mails
   belongs_to :manager
-  
+
   has_many :sent_employee_messages, :foreign_key => 'sender_id', class_name: "EmployeeMessage"
   has_many :sent_employee_messages_to, through: :sent_employee_messages, source: :receiver
 
