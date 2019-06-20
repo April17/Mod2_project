8774e2ea652e0c723237403efe5391b0c1265628
Yonas Fesehatsion <yone920@gmail.com>
2019-06-20 15:18:13 -0400

    Style added

--- app/controllers/common_pages_controller.rb
+++ app/controllers/common_pages_controller.rb
@@ -4,11 +4,17 @@ class CommonPagesController < ApplicationController
     username = params[:username]
     if username[0..2] == "emp"
       @user = Employee.find_by(username: username)
+      @employeetasks = @user.employee_tasks
       @quote = Faker::Games::LeagueOfLegends.quote
+      @coworkers = @user.manager.employees - [@user]
     elsif username[0..2] == "mag"
-      @user = Manager.find_by(username: username)
+      @manager = Manager.find_by(username: username)
       @quote = Faker::TvShows::GameOfThrones.quote
+      
+      render template: "common_pages/mag_show" 
     end
   end
 
 end
+
+
