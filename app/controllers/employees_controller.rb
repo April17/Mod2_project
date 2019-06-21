ac2a9524e293ccdcc52e46e0b39e56d4694f7ba2
April17 <fanmeng0417@gmail.com>
2019-06-21 13:09:45 -0400

    Back end update

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,15 +1,13 @@
 class EmployeesController < ApplicationController
   before_action :find_employee
-  skip_before_action :authorized?, only: [:new, :create]
+  skip_before_action :authorized?, only: [:new, :create, :destroy]
   def show
     if @employee.username == session[:user_username]
       @employeetasks = @employee.employee_tasks
-
       @employee_messages = @employee.received_employee_messages
       @employee_manager_messages = @employee.mails.select {|mail| mail.sender != @employee.username}
       @employee_sent_messages = @employee.sent_employee_messages
       @employee_sent_manager_messages = @employee.mails.select {|mail| mail.sender == @employee.username}
-
       @coworkers = @employee.manager.employees - [@employee]
       message_list_temp = @employee.manager.employees - [@employee]
       @message_list = message_list_temp
@@ -27,16 +25,47 @@ class EmployeesController < ApplicationController
   def create
     # byebug
     params[:employee][:username] = "emp" << params[:employee][:username]
-    employee = Employee.create(employee_params)
+    employee = Employee.create(employee_params(:name, :username, :password, :manager_id))
     redirect_to new_login_path
   end
 
-  def employee_params
-    params.require(:employee).permit(:name, :username, :password, :manager_id)
+  def edit
+
   end
 
+  def update
+    if @employee.username == session[:user_username]
+      return redirect_to @user
+    end
+    password = params[:employee][:password]
+    password2 = params[:employee][:confirm_password]
+    if @utility.double_chack_passowrd(password, password2)
+      @employee.update(employee_params(:name, :password))
+      redirect_to @employee
+    else
+      flash[:messages] = "Password is not match"
+      redirect_to @edit_account_path
+    end
+  end
+
+  def employee_params(*argu)
+    params.require(:employee).permit(*argu)
+  end
+
+  def got_fired
+    employee = Employee.find_by(id: params[:employee_id])
+    employee.destroy
+    redirect_to edit_employees_path(Manager.find_by(username: session[:user_username]))
+  end
+
+  def destroy
+    @employee.destroy
+    session[:user_username] = nil
+    redirect_to "/"
+  end
 end
 
 def find_employee
   @employee = Employee.find_by(id: params[:id])
+  @quote = Faker::Games::LeagueOfLegends.quote
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   post "/message_redirect", to: "common_pages#message_redirect", as: "message_redirect"
   get "/logout", to: "login#destroy", as: "logout"
   get "/show/:username", to: "common_pages#show", as: "common_show"
+  get "/managers/:id/edit_employees", to: "managers#edit_employees", as: "edit_employees"
+  post "/employees/got_fired", to: "employees#got_fired", as: "got_fired"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
